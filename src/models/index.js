6c03de8427e024ff88c5198abe0f7987ae54ccfd
DAVE.TUSIA@GMAIL.COM~ <DAVE.TUSIA@GMAIL.COM>
2022-09-29 17:38:24 -0700

    Roles and token working

--- src/models/index.js
+++ src/models/index.js
@@ -10,6 +10,8 @@ const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 const sequelize = new Sequelize(DATABASE_URL);
 const reservations = reservationModel(sequelize, DataTypes);
 
+
+
 module.exports = {
   db: sequelize,
   users: userModel(sequelize, DataTypes),
